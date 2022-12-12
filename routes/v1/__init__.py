6f56b9c7e1a29292852a826da609936ff95155e7
smoo <tmddn3070@gmail.com>
2022-12-12 23:04:43 +0900

    Changes

--- routes/v1/__init__.py
+++ routes/v1/__init__.py
@@ -4,9 +4,11 @@ from fastapi import APIRouter
 from .getlink import getlink_route
 from .createlink import create_route
 from .getlinkinfo import linkinfo_route
+from .used import link_used_info_route
 v1_router = APIRouter()
 
 # v1_router.include_router(users_router, prefix="/users", tags=["users"])
 v1_router.include_router(getlink_route, prefix="/link", tags=["link"])
 v1_router.include_router(create_route, prefix="/link", tags=["link"])
 v1_router.include_router(linkinfo_route, prefix="/info", tags=["link"])
+v1_router.include_router(link_used_info_route, prefix="/used", tags=["link"])
---
+++ routes/v1/used.py
@@ -0,0 +1,29 @@
+import motor as motor
+from fastapi import APIRouter, HTTPException
+from starlette.responses import JSONResponse
+from models.link3 import GetLinkinfo
+
+link_used_info_route = APIRouter()
+
+@link_used_info_route.get("/")
+async def get_link(link: str):
+    try:
+        mongo_client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://172.30.1.26:27017')
+    except Exception as e:
+        print(e)
+        return JSONResponse(status_code=500, content={"error": "Internal Server Error"})
+    db = mongo_client['linkl']
+    collection = db['logs']
+    try:
+        """limit query to 50"""
+        log = await collection.find({"link": link}).limit(50).to_list(length=50)
+    except Exception as e:
+        print(e)
+        return JSONResponse(status_code=500, content={"error": "Internal Server Error"})
+    if log is None:
+        raise HTTPException(status_code=404, detail="Link not found")
+    """return log list"""
+    result = []
+    for i in log:
+        result.append({"ip": i['ip'], "time": i['time']})
+    return JSONResponse(status_code=200, content={"link": link, "log": result})
